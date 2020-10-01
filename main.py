9c654f351dd5fca3b32650843f09c0856c02516a
Acero Liang <swirlycone@Aceros-MacBook-Pro.local>
2020-10-01 17:57:21 -0400

    got the prices to show

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 URL = 'https://ca.finance.yahoo.com/quote/fcel'
+URL2 = 'https://ca.finance.yahoo.com/quote/aapl'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 #<div class=”My(6px) Pos(r) smartphone_Mt(6px)”>
-results = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})
-print(results.prettify())
+results = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
 
-# big dik
+page2 = requests.get(URL2)
+soup2 = BeautifulSoup(page2.content,'html.parser')
+results2 = soup2.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
+
+print(results)
+print(results2)
