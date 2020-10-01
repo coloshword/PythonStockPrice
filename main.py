5b99dd8d29953d59f24156d1149d5a0899b45d54
swirlycone <aceliang2001@gmail.com>
2020-10-01 17:22:09 -0400

    Got the HTML code down

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://ca.finance.yahoo.com/quote/fcel'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+#<div class=”My(6px) Pos(r) smartphone_Mt(6px)”>
+results = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})
+print(results.prettify())
+
