976cf4f3afeb5c86a5366d64380ad451f6ed78cd
Acero Liang <swirlycone@Aceros-MacBook-Pro.local>
2020-10-02 01:48:29 -0400

    Stock addinng function and a main loop to get values

--- add.py
+++ add.py
@@ -1,7 +1,18 @@
 def add_a_stock():
+    urls = []
     base_url = 'https://ca.finance.yahoo.com/quote/'
     company = input("Enter the stock symbol: ")
-    url = base_url + company
-    print(company)
+    while company != "":
+        url = base_url + company
+        company = input("Enter the stock symbol: ")
+        urls.append(url)
+    for url in urls:
+        save_data(url)
+
+
+def save_data(text):
+    with open("stock_dictionary.txt", 'a') as file_body:
+        lines = text + '\n'
+        file_body.write(lines)
 
 add_a_stock()
--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-URL = 'https://ca.finance.yahoo.com/quote/fcel'
-URL2 = 'https://ca.finance.yahoo.com/quote/aapl'
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-#<div class=”My(6px) Pos(r) smartphone_Mt(6px)”>
-results = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
+def main():
+    with open('stock_dictionary.txt', 'r') as fh:
+        for URL in fh.readlines():
+            page = requests.get(URL.replace('\n', ''))
+            soup = BeautifulSoup(page.content, 'html.parser')
+            results = soup.find('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
+            print(results)
 
-page2 = requests.get(URL2)
-soup2 = BeautifulSoup(page2.content,'html.parser')
-results2 = soup2.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
-
-print(results)
-print(results2)
+while True:
+    main()
+    time.sleep(60)
