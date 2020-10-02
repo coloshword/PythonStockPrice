9816b0dec5696f94de888390bf07dee9df5da2d6
Acero Liang <aceliang2001@gmail.com>
2020-10-02 13:19:14 -0400

    Added an option to remove stocks from the checking

--- add.py
+++ add.py
@@ -2,8 +2,11 @@ def add_a_stock():
     urls = []
     prices = []
     base_url = 'https://ca.finance.yahoo.com/quote/'
-    company = input("Enter the stock symbol: ")
+    company = input("Enter the stock symbol, enter - if you want to remove a stock: ")
     while company != "":
+        if company == "-":
+            remove_company()
+            break
         price = input("Enter the price you want to sell at: ")
         url = base_url + company
         urls.append(url)
@@ -13,9 +16,22 @@ def add_a_stock():
         save_data(urls[i], prices[i])
 
 
+def remove_company():
+    remove = input("Enter the symbol of the company you want to remove: ")
+    with open("stock_holds.txt", 'r+') as file_body:
+        lines = file_body.readlines()
+        for i in range(len(lines)):
+            if remove in lines[i]:
+                lines.pop(i)
+                file_body.truncate(0)
+                file_body.write("".join(lines))
+                break
+
+
 def save_data(stock, price):
     with open("stock_holds.txt", 'a') as file_body:
         lines = stock + " : " + price + '\n'
         file_body.write(lines)
+
 
 add_a_stock()
