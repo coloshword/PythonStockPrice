d557e8e3b6a2371ee9fd6ecb60d888e10ebc6ed9
Acero Liang <swirlycone@Aceros-MacBook-Pro.local>
2020-10-02 11:59:14 -0400

    Added a dictionary

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import os
+stock_dict = {}
+
+
+def create_dict():
+    global stock_dict
+    with open('stock_holds.txt', 'r') as fh:
+        for line in fh.readlines():
+            line = line.replace('\n', '')
+            url_price = line.split(':')
+            stock_dict[url_price[0] + ":" + url_price[1]] = float(url_price[2])
+
 
 def main():
-    with open('stock_holds.txt', 'r') as fh:
-        for URL in fh.readlines():
-            page = requests.get(URL.replace('\n', ''))
-            soup = BeautifulSoup(page.content, 'html.parser')
-            results = soup.find('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
-            header = URL[35:].replace('\n', "")
-            print(header + " : $" + results)
+    create_dict()
+    for key in stock_dict:
+        page = requests.get(key.replace(' ', ''))
+        soup = BeautifulSoup(page.content, 'html.parser')
+        results = soup.find('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
+        header = key[35:].replace('\n', "")
+        print(header + " : $" + results)
 
 
 def notify(title, text):
