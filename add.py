e3a288bebc1e009415a6160bb4887ef048fbb80d
Acero Liang <swirlycone@Aceros-MacBook-Pro.local>
2020-10-02 11:26:16 -0400

    Added desired price system and a notification system

--- add.py
+++ add.py
@@ -1,18 +1,21 @@
 def add_a_stock():
     urls = []
+    prices = []
     base_url = 'https://ca.finance.yahoo.com/quote/'
     company = input("Enter the stock symbol: ")
     while company != "":
+        price = input("Enter the price you want to sell at: ")
         url = base_url + company
-        company = input("Enter the stock symbol: ")
         urls.append(url)
-    for url in urls:
-        save_data(url)
+        prices.append(price)
+        company = input("Enter the stock symbol: ")
+    for i in range(len(urls)):
+        save_data(urls[i], prices[i])
 
 
-def save_data(text):
-    with open("stock_dictionary.txt", 'a') as file_body:
-        lines = text + '\n'
+def save_data(stock, price):
+    with open("stock_holds.txt", 'a') as file_body:
+        lines = stock + " : " + price + '\n'
         file_body.write(lines)
 
 add_a_stock()
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-
+import os
 
 def main():
-    with open('stock_dictionary.txt', 'r') as fh:
+    with open('stock_holds.txt', 'r') as fh:
         for URL in fh.readlines():
             page = requests.get(URL.replace('\n', ''))
             soup = BeautifulSoup(page.content, 'html.parser')
@@ -13,7 +13,12 @@ def main():
             print(header + " : $" + results)
 
 
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+
 while True:
     print('-' * 30)
     main()
-    time.sleep(60)
+    time.sleep(30)
