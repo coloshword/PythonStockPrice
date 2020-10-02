822f737e5bf2bbe3bbc92ca5f744990b869ff149
Acero Liang <aceliang2001@gmail.com>
2020-10-02 13:49:24 -0400

    Changed create_dict function to include other text files

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ from bs4 import BeautifulSoup
 import time
 import os
 stock_dict = {}
+buy_dict = {}
 
 
-def create_dict():
+def create_dict(text_file):
     global stock_dict
-    stock_dict = {}
-    with open('stock_holds.txt', 'r') as fh:
+    global buy_dict
+    if text_file == "stock_dict":
+        stock_dict = {}
+    else:
+        buy_dict = {}
+    with open(text_file, 'r') as fh:
         for line in fh.readlines():
             line = line.replace('\n', '')
             url_price = line.split(':')
             stock_dict[url_price[0] + ":" + url_price[1]] = float(url_price[2])
 
 
-def main():
-    create_dict()
+def check_holds():
+    create_dict('stock_holds.txt')
     for key in stock_dict:
         page = requests.get(key.replace(' ', ''))
         soup = BeautifulSoup(page.content, 'html.parser')
@@ -24,6 +29,9 @@ def main():
         check_value_sell(key, float(price_now))
         header = key[35:].replace(' ', "")
         print(header + " : $" + price_now)
+
+#
+# def check_buys():
 
 
 def notify(title, text):
@@ -40,6 +48,6 @@ def check_value_sell(company, current_price):
 
 
 while True:
-    print('-' * 30)
-    main()
+    print('-' * 15, "CURRENT STOCKS IN POSSESSION", '-' * 15)
+    check_holds()
     time.sleep(30)
